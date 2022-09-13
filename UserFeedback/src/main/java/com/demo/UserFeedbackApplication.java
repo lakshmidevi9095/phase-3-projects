2ac98894053f87baeed95b9c4f42d3bde06ffcb2
lakshmidevi9095 <109669876+lakshmidevi9095@users.noreply.github.com>
2022-09-13 11:06:00 +0530

    Add files via upload

---
+++ UserFeedback/src/main/java/com/demo/UserFeedbackApplication.java
@@ -0,0 +1,14 @@
+package com.demo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.demo")
+public class UserFeedbackApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(UserFeedbackApplication.class, args);
+	}
+
+
+}
